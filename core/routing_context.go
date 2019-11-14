604a5fdcedd3841e4e0425d85b0160207e65439a
Markus Sommer <msommer@informatik.uni-marburg.de>
2019-11-14 16:50:09 +0100

    CONTEXT: Send context data to peers

--- core/routing_context.go
+++ core/routing_context.go
@@ -3,9 +3,12 @@ package core
 import (
 	"encoding/json"
 	"github.com/dop251/goja"
+	"github.com/dtn7/cboring"
+	"github.com/dtn7/dtn7-go/bundle"
 	"github.com/dtn7/dtn7-go/cla"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"sync"
@@ -18,6 +21,8 @@ type ContextRouting struct {
 	// context is this node's context information.
 	// The map key is the name of information, the value has to be a JSON-encoded string
 	context map[string]string
+	// contains peer context data, the keys are nodeIDs, and the values are the same construct as our own context map
+	peerContext map[string]map[string]string
 	// javascriptVM is the javascript interpreter which executes the context evaluation-code
 	javascriptVM *goja.Runtime
 	// the javascript interpreter is not thread safe, but reinstantiating it might be a bit much of an overhead
@@ -30,6 +35,7 @@ func NewContextRouting(c *Core) *ContextRouting {
 	contextRouting := ContextRouting{
 		c:            c,
 		context:      make(map[string]string),
+		peerContext:  make(map[string]map[string]string),
 		javascriptVM: goja.New(),
 	}
 
@@ -50,12 +56,48 @@ func NewContextRouting(c *Core) *ContextRouting {
 	contextRouting.javascriptVM.ToValue(nodeID)
 	log.Info("Finished initialising javascript interpreter")
 
+	// register our custom metadata-block
+	extensionBlockManager := bundle.GetExtensionBlockManager()
+	if !extensionBlockManager.IsKnown(ExtBlockTypeContextBlock) {
+		// since we already checked if the block type exists, this really shouldn't ever fail...
+		_ = extensionBlockManager.Register(newContextBlock(contextRouting.context))
+	}
+
 	log.Info("Finished initialising ContextRouting")
 	return &contextRouting
 }
 
 func (contextRouting *ContextRouting) NotifyIncoming(bp BundlePack) {
-	// TODO: Dummy Implementation
+	bndl, err := bp.Bundle()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Warn("Couldn't get bundle data")
+		return
+	}
+
+	// handle context bundles
+	if metaDataBlock, err := bndl.ExtensionBlock(ExtBlockTypeProphetBlock); err == nil {
+		// if it's from us, do nothing
+		if bndl.PrimaryBlock.SourceNode == contextRouting.c.NodeId {
+			return
+		}
+
+		peerID := bndl.PrimaryBlock.SourceNode
+
+		log.WithFields(log.Fields{
+			"source": peerID,
+		}).Debug("Received peer context")
+
+		contextBlock := metaDataBlock.Value.(*ContextBlock)
+		peerContext := contextBlock.getContext()
+
+		contextRouting.contextSemaphore.Lock()
+		contextRouting.peerContext[peerID.String()] = peerContext
+		contextRouting.contextSemaphore.Unlock()
+
+		return
+	}
 }
 
 func (contextRouting *ContextRouting) DispatchingAllowed(bp BundlePack) bool {
@@ -64,12 +106,33 @@ func (contextRouting *ContextRouting) DispatchingAllowed(bp BundlePack) bool {
 }
 
 func (contextRouting *ContextRouting) SenderForBundle(bp BundlePack) (sender []cla.ConvergenceSender, delete bool) {
+	bndl, err := bp.Bundle()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Warn("Couldn't get bundle data")
+		return
+	}
+
+	// handle context bundles
+	if _, err := bndl.ExtensionBlock(ExtBlockTypeProphetBlock); err == nil {
+		if bndl.PrimaryBlock.SourceNode == contextRouting.c.NodeId {
+			// if we are the originator of this bundle, we forward it to everyone
+			return contextRouting.c.claManager.Sender(), false
+		} else {
+			// if this is another node's context message, we do not forward it and delete it immediately
+			return nil, true
+		}
+	}
+
 	contextRouting.contextSemaphore.RLock()
 	context := contextRouting.context
+	peerContext := contextRouting.peerContext
 	contextRouting.contextSemaphore.RUnlock()
 
 	contextRouting.vmSemaphore.Lock()
 	contextRouting.javascriptVM.ToValue(context)
+	contextRouting.javascriptVM.ToValue(peerContext)
 	contextRouting.vmSemaphore.Unlock()
 
 	return nil, false
@@ -135,4 +198,73 @@ func (contextRouting *ContextRouting) contextUpdateHandler(w http.ResponseWriter
 	log.WithFields(log.Fields{
 		"context": contextRouting.context,
 	}).Debug("CONTEXT_UPDATE: Successfully updated Context")
+}
+
+const ExtBlockTypeContextBlock uint64 = 35043
+
+type ContextBlock map[string]string
+
+func newContextBlock(context map[string]string) *ContextBlock {
+	contextBlock := ContextBlock(context)
+	return &contextBlock
+}
+
+func (contextBlock *ContextBlock) getContext() map[string]string {
+	return *contextBlock
+}
+
+func (contextBlock *ContextBlock) BlockTypeCode() uint64 {
+	return ExtBlockTypeContextBlock
+}
+
+func (contextBlock *ContextBlock) CheckValid() error {
+	return nil
+}
+
+func (contextBlock *ContextBlock) MarshalCbor(w io.Writer) error {
+	err := cboring.WriteMapPairLength(uint64(len(*contextBlock)), w)
+	if err != nil {
+		return err
+	}
+
+	for key, value := range *contextBlock {
+		err = cboring.WriteTextString(key, w)
+		if err != nil {
+			return err
+		}
+
+		err = cboring.WriteTextString(value, w)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (contextBlock *ContextBlock) UnmarshalCbor(r io.Reader) error {
+	length, err := cboring.ReadMapPairLength(r)
+	if err != nil {
+		return err
+	}
+
+	context := make(map[string]string, length)
+	var i uint64
+	for i = 0; i < length; i++ {
+		key, err := cboring.ReadTextString(r)
+		if err != nil {
+			return err
+		}
+
+		value, err := cboring.ReadTextString(r)
+		if err != nil {
+			return err
+		}
+
+		context[key] = value
+	}
+
+	*contextBlock = context
+
+	return nil
 }
